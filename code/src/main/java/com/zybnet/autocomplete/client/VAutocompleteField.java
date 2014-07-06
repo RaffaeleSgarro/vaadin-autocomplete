a42d9e3411b583b229885cfc5f20f80ccf0cce2b
Raffaele Sgarro <raffaelesgarro@gmail.com>
2014-07-07 01:10:29 +0200

    Fixed typo

--- code/src/main/java/com/zybnet/autocomplete/client/VAutocompleteField.java
+++ code/src/main/java/com/zybnet/autocomplete/client/VAutocompleteField.java
@@ -75,7 +75,7 @@ public class VAutocompleteField extends Composite {
 
         @Override
         public String getReplacementString() {
-          return src.getDisplayString() + ":::::ASDF";
+          return src.getDisplayString();
         }
       });
     }

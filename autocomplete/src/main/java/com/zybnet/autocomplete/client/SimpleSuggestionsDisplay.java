66ad047fbd3d53955c898bdba337aac81384fcbe
Raffaele Sgarro <raffaelesgarro@gmail.com>
2014-07-20 15:26:52 +0200

    Fixed popup placement il Horizontal and VerticalLayout

--- autocomplete/src/main/java/com/zybnet/autocomplete/client/SimpleSuggestionsDisplay.java
+++ autocomplete/src/main/java/com/zybnet/autocomplete/client/SimpleSuggestionsDisplay.java
@@ -2,8 +2,11 @@ package com.zybnet.autocomplete.client;
 
 
 import com.google.gwt.core.client.GWT;
+import com.google.gwt.core.client.Scheduler;
+import com.google.gwt.core.client.Scheduler.ScheduledCommand;
 import com.google.gwt.user.client.ui.PopupPanel;
 import com.google.gwt.user.client.ui.SuggestBox;
+import com.google.gwt.user.client.ui.SuggestOracle;
 import com.google.gwt.user.client.ui.Widget;
 import com.vaadin.client.ui.VOverlay;
 
@@ -23,5 +26,19 @@ public class SimpleSuggestionsDisplay extends SuggestBox.DefaultSuggestionDispla
     popup.setAutoHideEnabled(true);
     return popup;
   }
+  
+  @Override
+  public void showSuggestions(final SuggestBox suggestBox, final java.util.Collection<? extends SuggestOracle.Suggestion> suggestions, final boolean isDisplayStringHTML, final boolean isAutoSelectEnabled, final SuggestBox.SuggestionCallback callback) {
+    // This horrible hack is copied from VFilterSelect
+    // Apparently, Vaadin layouts mess with paddings and margins, so we need to
+    // wait until the layout code has executed before computing the left/top offsets
+    Scheduler.get().scheduleDeferred(new ScheduledCommand() {
+      @Override
+      public void execute() {
+        SimpleSuggestionsDisplay.super.showSuggestions(suggestBox, suggestions, isDisplayStringHTML, isAutoSelectEnabled, callback);
+      }
+    });
+  }
+
 
 }

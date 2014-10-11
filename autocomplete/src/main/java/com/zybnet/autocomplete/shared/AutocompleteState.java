bf0baaec690a379a370aa1c53669e7d09b54e641
Raffaele Sgarro <raffaelesgarro@gmail.com>
2014-10-11 20:57:21 +0200

    Resolved issue: SuggestBoxPopup will not open #3

--- autocomplete/src/main/java/com/zybnet/autocomplete/shared/AutocompleteState.java
+++ autocomplete/src/main/java/com/zybnet/autocomplete/shared/AutocompleteState.java
@@ -8,9 +8,10 @@ import com.vaadin.shared.annotations.DelegateToWidget;
 
 @SuppressWarnings("serial")
 public class AutocompleteState extends AbstractFieldState {
+  public int syncId;
+  public String text;
   public List<AutocompleteFieldSuggestion> suggestions = Collections.emptyList();
   public int delayMillis = 300;
-  public boolean hasFocus = false;
   @DelegateToWidget public int minimumQueryCharacters = 3;
   @DelegateToWidget public boolean trimQuery = true;
 }
